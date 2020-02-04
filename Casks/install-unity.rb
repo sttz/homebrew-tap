cf0fda4d1b173fcc2297ca5c76170647eef3ee2f
Adrian Stutz <adrian@sttz.ch>
2020-02-04 12:38:10 +0100

    Update install-unity to 2.7.1

--- Casks/install-unity.rb
+++ Casks/install-unity.rb
@@ -1,6 +1,6 @@
 cask 'install-unity' do
-  version '2.7.0'
-  sha256 '1068e5365163c018e9aa559e66216cb410b785ec33a3a62a95c2ec4f39f36c83'
+  version '2.7.1'
+  sha256 '58e11ce52e31a56e505851587690e5c3f41ec88909dfcfbd08b7f2941d1a9fd4'
 
   conflicts_with formula: 'install-unity'
 
--- Formula/install-unity.rb
+++ Formula/install-unity.rb
@@ -1,8 +1,8 @@
 class InstallUnity < Formula
   desc "Script to install Unity 3D versions from the command-line"
   homepage "https://github.com/sttz/install-unity"
-  url "https://github.com/sttz/install-unity/archive/2.7.0.tar.gz"
-  sha256 "bb0e066e02beadfab8e0788c9e1193600df6230f8035b4ea31dc535bb72bcfba"
+  url "https://github.com/sttz/install-unity/archive/2.7.1.tar.gz"
+  sha256 "3596a238afe9828290164084495e91dd9b3b2647d4b53c3c14fb2392b86e7b88"
   head "https://github.com/sttz/install-unity.git", :branch => "next"
 
   depends_on "mono"
