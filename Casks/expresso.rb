8a861d3053018b6f647aebc9539cf2c7ed8f5bf9
Adrian Stutz <adrian@sttz.ch>
2020-04-16 19:48:39 +0200

    Update expresso to 1.0.1

--- Casks/expresso.rb
+++ Casks/expresso.rb
@@ -1,6 +1,6 @@
 cask 'expresso' do
-  version '1.0.0'
-  sha256 'c39299cb6e6cfe92083726375329e241a6bc4c98061c9074c8619143a76edaa0'
+  version '1.0.1'
+  sha256 '36d26821d724738e3fa48423e7d60a3f80a72255f2dec4ff64c3b6babfd114d1'
 
   conflicts_with formula: 'expresso'
 
