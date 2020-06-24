ea2b51a2c3149d5766d3e3bd591508e8e5d970e7
Adrian Stutz <adrian@sttz.ch>
2020-06-24 13:36:45 +0200

    Update install-unity to 2.8.0

--- Casks/install-unity.rb
+++ Casks/install-unity.rb
@@ -1,6 +1,6 @@
 cask 'install-unity' do
-  version '2.7.2'
-  sha256 '0bd9681788894134b93bd4740944eab276c8bb82ccb475c29da248010df49ffd'
+  version '2.8.0'
+  sha256 '726f1b3b0c3a7dfda8f17735ba3cabd8325d0fd130afe8c030b7bb788e1b6bed'
 
   conflicts_with formula: 'install-unity'
 
--- Formula/install-unity.rb
+++ Formula/install-unity.rb
@@ -1,8 +1,8 @@
 class InstallUnity < Formula
   desc "Script to install Unity 3D versions from the command-line"
   homepage "https://github.com/sttz/install-unity"
-  url "https://github.com/sttz/install-unity/archive/2.7.2.tar.gz"
-  sha256 "3b9b329d9d1924a364b4a8c5d821bf3286c4c9e9ca056f1c476837c336b77cf6"
+  url "https://github.com/sttz/install-unity/archive/2.8.0.tar.gz"
+  sha256 "e6c687e47aeef1ebaad4b0508726eb2b37c7682ac65c524f3f2078e0bf784196"
   head "https://github.com/sttz/install-unity.git", :branch => "next"
 
   depends_on "mono"
