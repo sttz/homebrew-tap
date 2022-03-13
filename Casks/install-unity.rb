1ac7281aa515c200e2d13bd9846ce542f29bc363
Adrian Stutz <adrian@sttz.ch>
2022-03-13 17:48:16 +0100

    Update install-unity Cask to 2.10.0 (Formula needs to be tested)

--- Casks/install-unity.rb
+++ Casks/install-unity.rb
@@ -1,6 +1,6 @@
 cask 'install-unity' do
-  version '2.9.0'
-  sha256 'ceeceb3f4345db36628f0772d23e1460a0de97769e719c446cf0e13e57ea746f'
+  version '2.10.0'
+  sha256 '9b5e469692c690f04ac73aec32cfeacc2e33f308f3046d4dc178c1ab68db2ed4'
 
   conflicts_with formula: 'install-unity'
 
