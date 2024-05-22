4758776459e890bddd77070f4a5a857d144f2181
Adrian Stutz <adrian@sttz.ch>
2024-05-22 16:08:35 +0200

    Fix install-unity source archive hash

--- Formula/install-unity.rb
+++ Formula/install-unity.rb
@@ -2,7 +2,7 @@ class InstallUnity < Formula
   desc "Script to install Unity 3D versions from the command-line"
   homepage "https://github.com/sttz/install-unity"
   url "https://github.com/sttz/install-unity/archive/2.12.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "2553534fb004971df3a614a0a5a3ac788c0eead5f25ef6b74eb6a484e52913df"
   head "https://github.com/sttz/install-unity.git", branch: "next"
 
   depends_on "dotnet"
