9e6d316641e751b971aa652192441d02b4547c09
Adrian Stutz <adrian@sttz.ch>
2026-03-15 16:14:57 +0100

    Remove conflicts_with, which has been removed without replacement

--- Casks/expresso.rb
+++ Casks/expresso.rb
@@ -2,8 +2,6 @@ cask 'expresso' do
   version '1.3.0'
   sha256 '683e9084f1448e03b959c7256933108c6f486e0e06e608ef3b678e651b8357b2'
 
-  conflicts_with formula: 'expresso'
-
   url "https://github.com/sttz/expresso/releases/download/#{version}/expresso-#{version}-osx64.zip"
   name 'expresso'
   homepage 'https://github.com/sttz/expresso/'
--- Casks/install-unity.rb
+++ Casks/install-unity.rb
@@ -2,8 +2,6 @@ cask 'install-unity' do
   version '2.13.1'
   sha256 '43b27a1c9cd01203541aa4aa7162f2af2800bcd27af636787107a2cfdf18d0cb'
 
-  conflicts_with formula: 'install-unity'
-
   url "https://github.com/sttz/install-unity/releases/download/#{version}/install-unity-#{version}.zip"
   name 'install-unity'
   homepage 'https://github.com/sttz/install-unity/'
